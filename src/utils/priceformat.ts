15f42de1f1c8f6844aad1591462a2dbd516037a3
Daniel-Musunza <musunzafestus2019@gmail.com>
2024-11-24 10:54:12 +0300

    export function formatNumberWithCommas(number: any) {

--- src/utils/priceformat.ts
+++ src/utils/priceformat.ts
@@ -1,4 +1,4 @@
-export function formatNumberWithCommas(number) {
+export function formatNumberWithCommas(number: any) {
     // Ensure the input is a number by converting the string to a number
     return Number(number).toLocaleString();
 }
